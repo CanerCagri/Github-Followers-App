a5b6717ddd87027b2efea22724b444de9378b84f
CanerCagri <canerrcagrii@gmail.com>
2022-07-17 21:17:04 +0300

    Custom button and custom textfield added.

---
+++ Github-Followers-App/GFButton.swift
@@ -0,0 +1,34 @@
+//
+//  GFButton.swift
+//  Github-Followers-App
+//
+//  Created by Caner Çağrı on 17.07.2022.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(title: String, backgroundColor: UIColor) {
+        super.init(frame: .zero)
+        self.setTitle(title, for: .normal)
+        self.backgroundColor = backgroundColor
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ Github-Followers-App/GFTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GFTextField.swift
+//  Github-Followers-App
+//
+//  Created by Caner Çağrı on 17.07.2022.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        layer.cornerRadius = 10
+        
+        //Label color is making in light mode "black", in dark mode "white"
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a username"
+        
+        
+    }
+}
